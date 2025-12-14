cac107a3fbf352e439f9f019971d70c520e40dd1
jaovitim <162746128+jaovitim590@users.noreply.github.com>
2025-12-14 01:19:12 -0300

    changing for only frontend

--- back/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-  SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
-  GEOCODE = "https://geocoding-api.open-meteo.com/v1/search"
-  WEATHER = "https://api.open-meteo.com/v1/forecast"
